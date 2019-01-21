e9204e96814b757faaa9687737cef9ee9dec56a3
somycho1 <soumyadeep123456@gmail.com>
2019-01-21 15:18:46 +0530

    h2console

---
+++ src/main/java/com/stackroute/muzixApplicationTask1/repository/TrackRepository.java
@@ -0,0 +1,17 @@
+package com.stackroute.muzixApplicationTask1.repository;
+
+import com.stackroute.muzixApplicationTask1.domain.Track;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+//import org.springframework.data.mongodb.repository.MongoRepository;
+
+import org.springframework.stereotype.Repository;
+
+import java.util.List;
+
+@Repository
+//public interface TrackRepository extends MongoRepository<Track,Integer> {
+public interface TrackRepository extends JpaRepository<Track,Integer> {
+   @Query("SELECT t FROM Track t WHERE t.trackName = ?1")
+    public List<Track> findBytrackName(String name);
+}
